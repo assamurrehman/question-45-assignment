8e36c0af6e7d07d914bc6d61dd36c2874dbf081a
assamurrehman <159828947+assamurrehman@users.noreply.github.com>
2024-05-28 23:55:12 +0500

    Add files via upload

---
+++ 4.ts
@@ -0,0 +1,3 @@
+let quote:string="a person who never made a mistake never tried any thing new";
+let author:string="alber einstien";
+console.log(`${author} once said, "${quote}`);
---
+++ app 4.js
@@ -0,0 +1,3 @@
+var quote = "a person who never made a mistake never tried any thing new";
+var author = "alber einstien";
+console.log("".concat(author, " once said, \"").concat(quote));
