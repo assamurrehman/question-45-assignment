8a55727143dccf9f7f1fb9e7088c1de4c030c4b9
assamurrehman <159828947+assamurrehman@users.noreply.github.com>
2024-05-28 23:58:48 +0500

    Add files via upload

---
+++ 9,10.js
@@ -0,0 +1,5 @@
+var favouriteNumber = 7;
+console.log("my fav number is ".concat(favouriteNumber));
+// this also cover question number 10 adding coments
+//by this typescript will not read and compile this comment
+//this program prints my favourite number which is 7tsc app.ts
---
+++ 9,10.ts
@@ -0,0 +1,6 @@
+let favouriteNumber:number=7;
+console.log(`my fav number is ${favouriteNumber}`);
+// this also cover question number 10 adding coments
+//by this typescript will not read and compile this comment
+//this program prints my favourite number which is 7tsc app.ts
+
